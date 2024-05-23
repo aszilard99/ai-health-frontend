4c63b570957048fed6e01380e08acb3e8bf2ea1b
Albert-Toth <aszilard99@gmail.com>
2024-05-23 21:26:01 +0300

    WIP d3 linear gauge implementation

--- src/app/components/LinearGauge.js
+++ src/app/components/LinearGauge.js
@@ -1,31 +1,56 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState, useLayoutEffect } from "react";
 import * as d3 from 'd3';
 import '../linear-gauge.css'
 
+
+const useWindowSize = () => {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+      const updateSize = () => {
+        setSize([window.innerWidth, window.innerHeight]);
+        d3.selectAll('svg').remove();
+      };
+      window.addEventListener("resize", updateSize);
+      return () => window.removeEventListener("resize", updateSize);
+    }, []);
+    return size;
+  };
+
 export default function LinearGauge() {
-    
+
+    const [width2, height2] = useWindowSize();
+
+    const containerRef = useRef(null);
+    var container;
+    var width;
+    var height;
+    var chart_w;
+    var resultPos;
+    var text_margins;
+    var chart_y_pos;
+    var height = 30;
+    var result = 0.99;
+
     useEffect(()=> {
-        var container = d3.select('#linear-gauge');
-        var width = parseFloat(container.style("width"));
+        container = d3.select(containerRef.current);
+        width = parseFloat(container.style("width"));
         var gaugeScale = [
             { "color": "#00CC96", "value": 0.227, "depth": 0, "risk": "no risk"},
             { "color": "#00E7AA", "value": 0.380, "depth": 1,"risk": "low risk"},
             { "color": "#FFAA00", "value": 0.260, "depth": 2,"risk": "somewhat risk"},
             { "color": "#FF0000", "value": 0.133, "depth": 3, "risk": "hight risk"},
         ]
-        var result = 0.99;
-        var gauge_height = 30;
-        var chart_w = document.getElementById('linear-gauge').offsetWidth;
-        var resultPos = isNaN(result) ? chart_w * 1 : chart_w * result;
-        var text_margins = {
-            top: chart_y_pos + gauge_height + 35,
+        
+        chart_w = containerRef.current.clientWidth;
+        resultPos = isNaN(result) ? chart_w * 1 : chart_w * result;
+        text_margins = {
+            top: chart_y_pos + height + 35,
             right: 10,
             bottom: 0,
             left: 10
         };
-        // Tick mark
         var LF = 30;
-        var chart_y_pos = 0;
+        chart_y_pos = 0;
 
         var scaleValue = gaugeScale.sort((a, b) => parseFloat(a.depth) - parseFloat(b.depth)).map(obj => obj.value);
         var scaleColor = gaugeScale.sort((a, b) => parseFloat(a.depth) - parseFloat(b.depth)).map(obj => obj.color);
@@ -41,7 +66,6 @@ export default function LinearGauge() {
             .attr('y1', '0%').attr('x1', '0%')
             .attr('y2', '0%').attr('x2', '100%');
 
-        //Append multiple color stops by using D3's data/enter step
         var offset = 0;
         linearGradient.selectAll("stop")
             .data(color.range())
@@ -62,14 +86,14 @@ export default function LinearGauge() {
             .attr("rx", 6)
             .attr("ry", 6)
             .attr('width', width)
-            .attr('height', gauge_height)
+            .attr('height', height)
             .style('fill', 'url(#linear-gradient)');
 
         svg.append("g")
         .append("text")
         .classed("gaugeLabel", true)
-        .attr("x", chart_w - 30) // to adapt
-        .attr("y", (gauge_height + chart_y_pos) / 1.3) //text_margins.top )
+        .attr("x", chart_w - 30)
+        .attr("y", (height + chart_y_pos) / 1.3) 
         .attr("text-anchor", "end").text(gaugeScale[3].risk);
 
         var tickMark = svg.append("g");
@@ -78,26 +102,26 @@ export default function LinearGauge() {
             .attr("x1", resultPos)
             .attr("y1", chart_y_pos)
             .attr("x2", resultPos)
-            .attr("y2", gauge_height + chart_y_pos)
+            .attr("y2", height + chart_y_pos)
             .attr("stroke-width", 2)
             .attr("stroke", "black");
 
         tickMark.append("circle")
             .transition()
             .attr("cx", resultPos)
-            .attr("cy", (gauge_height + chart_y_pos) / 2)
+            .attr("cy", (height + chart_y_pos) / 2)
             .attr("r", 5);
 
         tickMark.append("text")
             .transition()
             .attr("x", resultPos / 0.95)
-            .attr("y", (gauge_height + chart_y_pos) / 1.2)
+            .attr("y", (height + chart_y_pos) / 1.2)
             .text(Math.round((result * 100) * 100) / 100 + " %");
-    }, []);
+    });
 
     return(
         <div>
-            <div id="linear-gauge">
+            <div id="linear-gauge" ref={containerRef}>
             </div>
             Linear gauge
         </div>
